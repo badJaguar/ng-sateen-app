66e4251d31f65765749a7430d087f9aff182be27
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2020-01-18 18:33:57 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { NgtUniversalModule } from '@ng-toolkit/universal';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    CommonModule,
+    NgtUniversalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
